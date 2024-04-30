3de401401a3dca5ae27c78c3665c2f2f0e8aba2e
sh20raj <shaswatraj3@gmail.com>
2024-05-01 00:10:36 +0530

    Added Tidio

--- src/components/chatbot.js
+++ src/components/chatbot.js
@@ -2,8 +2,9 @@
 export default function Chatbot() {
   return (
     <div dangerouslySetInnerHTML={{ __html : `
-    <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
-<script src="https://mediafiles.botpress.cloud/df70a6a6-f64e-4e3c-b200-8bfdba248df6/webchat/config.js" defer></script>
+    <script src="//code.tidio.co/eaownzjcrh9bw1ryihgqgvkm70ufj2sg.js" async></script>
+
+
 `}}>
       
     </div>
